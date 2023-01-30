37b81d07d5c93a3466ef0cd1599eaedada97ce26
Kolahalan R <kolahalan@psibertech.net>
2023-01-30 14:52:23 +0530

    Doughnut Chart Updated

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,46 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Chart from "fusioncharts/fusioncharts.charts";
+
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy"; //theme.candy should match with the name theme:"theme name"  in chart config
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, FusionTheme, Chart);
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        //Set the chart caption
+        caption: "Stars per language",
+
+        decimals: 0, //This to stop displaying decimals in a chart. By default, this wont be there and the chart will show decimals
+        doughnutRadius: "35%", //Increases the overall radius of piechart
+        showPercentValues: 0, //Restricts the % symbol added to data by default as in piechart
+        //paletteColors: "#f0db4f", //You can specify your own colours in this field
+        theme: "candy", //This data should match with the theme.themename extention in FusionTheme in line 14
+      },
+      // Chart Data
+      data: data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Doughnut2d;
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,49 +4,75 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  //console.log(repos);
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  console.log(repos);
+
+  //We are using repos.reduce(()=>{callback func},{}) to iterate the repos array,
+  //collect the data that we need and create a new array named 'languages' in our own sub fields
+
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item; //These values are coming from repos Array
     if (!language) {
       return total; //At times, the language field comes as null, at that time, we just return total
     }
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
+
     return total;
   }, {});
+  console.log(languages);
 
-  languages = Object.values(languages)
+  //Most used languages
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       //sorting array from highest to lowest with respect to value field type
       return b.value - a.value;
     })
     .slice(0, 5); //restricting the array ]to a length of 5
 
-  const chartdata = [
-    {
-      label: "HTML",
-      value: "13",
-    },
-    {
-      label: "CSS",
-      value: "27",
-    },
-    {
-      label: "Javascript",
-      value: "60",
-    },
-  ];
+  // most stars per language
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      //sorting array from highest to lowest with respect to stars field type
+
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+      //We are doing this map iteration so that we get the data in a {label: ,value: }
+      //format which is the way it should be sent to the chart
+    })
+    .slice(0, 5); //restricting the array ]to a length of 5
+
+  //The chart data for fusion charts need to in the format of label and value
+  // Sample chartdata for Fusion Charts
+  //  const chartdata = [
+  //   {
+  //     label: "HTML",
+  //     value: "13",
+  //   },
+  //   {
+  //     label: "CSS",
+  //     value: "27",
+  //   },
+  //   {
+  //     label: "Javascript",
+  //     value: "60",
+  //   },
+  // ];
   return (
     <section className="section">
       <Wrapper className="section-center">
         {/* <ExampleChart data={chartdata} />; */}
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
+        <div></div>
+        <Doughnut2D data={mostPopular} />
       </Wrapper>
     </section>
   );
